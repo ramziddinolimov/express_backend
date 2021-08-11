eeaedcfd7ef89243bdabd5a9976fb3007fcd58d2
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-12 01:19:10 +0500

    sidebar startes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const fs = require("fs").promises
 
 
 const app = express()
 
 
-app.listen(8088, () => {
-    console.log("App running at 8088");
-})
 
-app.get("/", (req, res) => {
-    res.send("Test");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", async (req, res) => {
+    let response = await fs.readFile(__dirname +  "/views/index.html", "utf-8");
+        res.send(response);
 });
+
+app.listen(3033, () => {
+    console.log("App running at 3033");
+})
