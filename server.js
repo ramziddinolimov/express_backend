5dd609b82f408ca4ba3727accb457f9b195d396b
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-12 00:18:03 +0500

    test

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express()
 
 
-app.listen(8080, () => {
-    console.log("App running at 8080");
+app.listen(8088, () => {
+    console.log("App running at 8088");
 })
 
-process.on("exit",  (event) => {
-    console.log(event);
-})
+app.get("/", (req, res) => {
+    res.send("Test");
+});
