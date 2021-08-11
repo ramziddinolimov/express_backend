483492be1cf37f81663fa8715f313a6d6cb58f37
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-11 23:58:17 +0500

    listen

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const server = express();
 
-server.listen(3030, () => console.log(`Server ready  at 3030`));
 
-server.use(middlewareForgetRequest);
+const app = express()
 
-server.get("/", (request, response) => {
-    if (!request.user) {
-    response.send("test");
-    }
-});
+
+app.listen(8080, () => {
+    console.log("App running at 8080");
+})
+
+process.on("exit",  (event) => {
+    console.log(event);
+})
