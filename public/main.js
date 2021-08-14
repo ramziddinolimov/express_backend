8264c2bac2c1b19b52c3c53798e0c2f3bf979405
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-14 23:21:59 +0500

    javascript started

---
+++ public/main.js
@@ -0,0 +1,39 @@
+
+const buttonElement1 = document.querySelector("#buttonStudents")
+const buttonElement2 = document.querySelector("#buttonCourses")
+const buttonElement3 = document.querySelector("#buttonSources")
+
+const inputElement = document.querySelector(".searchbox")
+const selectElement = document.querySelector(".navitem")
+
+
+
+
+
+
+
+buttonElement1.addEventListener('click', () => {
+    
+     inputElement.textContent = ""
+     selectElement.textContent = ""
+     console.log(buttonElement1);
+
+})
+
+
+
+
+
+buttonElement2.addEventListener('click', () => {
+    
+
+})
+
+
+
+
+
+buttonElement3.addEventListener('click', () => {
+    
+
+})
